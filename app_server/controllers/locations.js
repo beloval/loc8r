6ef7fc2f043e20106a3f9bfec107b5c6ab3543de
Aleksandr Belov <aleksandr.belov@pj.ca>
2016-08-12 15:05:57 -0400

    prototyping

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,9 @@
 /* GET HOME PAGE */
 module.exports.homelist = function (req, res){
-	res.render('index',{title :'Home list'});
+	res.render('locations-list',{title :'Home list'});
 };
 module.exports.locationInfo = function (req, res){
-	res.render('index',{title :'location info'});
+	res.render('locations-info',{title :'location info'});
 };
 module.exports.addReview = function (req, res){
 	res.render('index',{title :'add review'});
