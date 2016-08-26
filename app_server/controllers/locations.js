a038e7f870a72da55a9104a02b2e0f6bf921598e
Aleksandr Belov <aleksandr.belov@pj.ca>
2016-08-26 15:40:52 -0400

    extract data from views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,4 +1,5 @@
 /* GET HOME PAGE */
+
 module.exports.homelist = function (req, res) {
     res.render('locations-list', {
         title: 'Loc8r - find a  place to work with wifi',
@@ -35,12 +36,29 @@ module.exports.homelist = function (req, res) {
                 distance: '500m'
             }
 
-        ]
+        ],
+        sidebar : "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
 
     });
 };
 module.exports.locationInfo = function (req, res) {
-    res.render('locations-info', {title: 'location info'});
+    res.render('locations-info', {title: 'location info',
+        headername : 'Starcups',
+        location :{
+            name: 'Startcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+            distance: '100m'
+        },
+       openHoures : ['Monday - Friday 7:00am - 7:00pm',
+                     'Saturday 8:00am - 5:00pm',
+                     'Sunday closed'],
+       openHouresHeader : "Open houres:",
+        facilitiesHeader : 'facilities',
+        facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+
+    });
 };
 module.exports.addReview = function (req, res) {
     res.render('locations-review-form', {title: 'Add review'});
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,8 @@
 
 module.exports.about = function (req, res){
-	res.render('generic-text',{title :'About page'});
+	res.render('generic-text',{title :'About page',
+		abouttext : "Loc8r was created to help people find places to sit down and get a bit of work done."+
+		"|<br /><br />"+
+		"|So Alors write here something more!!!!'"
+	});
 };
