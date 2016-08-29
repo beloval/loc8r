c05533450f5ac7ffc607ae11caeb56af473e3667
Aleksandr Belov <aleksandr.belov@pj.ca>
2016-08-29 16:59:55 -0400

    added mongoose

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+requier('./app_server/models/db');
 
 var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
