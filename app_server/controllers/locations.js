c77a25663a5f44c15c9bfd8bbe3e60d92f204152
Aleksandr Belov <aleksandr.belov@pj.ca>
2016-10-20 15:58:19 -0400

    need to work in production

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ var apiOptions = {
 server : 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-apiOptions.server = "https://limitless-thicket-11066.herokuapp.com/";
+apiOptions.server = "https://limitless-thicket-11066.herokuapp.com";
 }
 var renderHomepage = function(req, res, data) {
     var message;
