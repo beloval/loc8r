fe5f601d3fbdc475cc2d17e223c3db7148989ad4
Aleksandr Belov <aleksandr.belov@pj.ca>
2017-02-03 13:39:42 -0500

    changes with angular

--- app_client/app.js
+++ app_client/app.js
@@ -1,12 +1,15 @@
+(function () {
 angular.module('loc8rApp', ['ngRoute']);
 function config ($routeProvider) {
-$routeProvider
-.when('/', {
-templateUrl: 'home/home.view.html', 
-controller: 'homeCtrl'
-})
-.otherwise({redirectTo: '/'});
+	$routeProvider
+	.when('/', {
+		templateUrl: 'home/home.view.html',
+		controller: 'homeCtrl',
+		controllerAs: 'vm'
+	})
+	.otherwise({redirectTo: '/'});
 }
 angular
 .module('loc8rApp')
 .config(['$routeProvider', config]);
+})();
